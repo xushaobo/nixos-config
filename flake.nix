1b0062eca87de9d0b53b2fd9fb9160771dcfb30d
name <email>
2024-11-01 08:06:44 +0800

    flake: add devShell

--- flake.nix
+++ flake.nix
@@ -45,5 +45,14 @@
           }
         ];
     };
+    devShells."x86_64-linux".default =
+      let
+        pkgs = nixpkgs.legacyPackages."x86_64-linux";
+      in
+      pkgs.mkShell {
+        packages = with pkgs; [
+          nixpkgs-fmt # TODO replace it with nixfmt-rfc-style
+        ];
+      };
   };
 }
